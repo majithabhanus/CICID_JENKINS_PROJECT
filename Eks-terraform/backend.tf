c70d48a0cd822c3395d5995699a76cd99371906e
majithabhanus <majithabhnaus@gmail.com>
2026-01-28 18:03:38 +0530

    2334

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "majimazz" # Replace with your actual S3 bucket name
+    bucket = "mazz123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1b"
+    region = "us-east-1"
   }
 }
--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1b"
+  region = "us-east-1"
 }
